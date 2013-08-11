9d7d13b64cea10433ae1326aace81e0da38e910d
Scott Zhuang <wsygzyx@mail.ustc.edu.cn>
2013-08-11 13:36:21 +0800

    first

--- finalmaybe/schedule/obj/Debug/newEvent.g.i.cs
+++ finalmaybe/schedule/obj/Debug/newEvent.g.i.cs
@@ -9,7 +9,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using MahApps.Metro.Controls;
 using System;
 using System.Diagnostics;
 using System.Windows;
